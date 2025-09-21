1aa7ec049ac944d96e7fd0090a399b346594f540
nostripledev <52100294+nostripledev@users.noreply.github.com>
2025-09-21 17:35:52 +0200

    Add compatibility definitions for logging and collision hints

--- vphysics_jolt/compat/compat_sdk2013.h
+++ vphysics_jolt/compat/compat_sdk2013.h
@@ -5,7 +5,9 @@
 // Source Engine branches for SDK 2013.
 
 #include "Color.h"
+#include "branch_overrides.h"
 
+#if !defined(GAME_GMOD_64X)
 enum LoggingSeverity_t
 {
     LS_MESSAGE = 0,
@@ -14,6 +16,16 @@ enum LoggingSeverity_t
     LS_ERROR = 3,
     LS_HIGHEST_SEVERITY = 4,
 };
+
+static const int MAX_LOGGING_MESSAGE_LENGTH = 2048;
+#else
+#undef DECLARE_LOGGING_CHANNEL
+#undef DEFINE_LOGGING_CHANNEL_NO_TAGS
+#undef Log_Msg
+#undef Log_Warning
+#undef Log_Error
+#undef InternalMsg
+#endif
 
 struct LoggingChannelInfo_t
 {
@@ -27,8 +39,6 @@ struct LoggingChannelInfo_t
 
 #define DEFINE_LOGGING_CHANNEL_NO_TAGS( Channel, ChannelName, ...  ) LoggingChannelInfo_t g_LoggingInfo##Channel = { ChannelName, __VA_ARGS__ };
 
-static const int MAX_LOGGING_MESSAGE_LENGTH = 2048;
-
 // TODO
 #define DevAssert( ... )
 #define DevAssertMsg( ... )
@@ -39,11 +49,13 @@ static const int MAX_LOGGING_MESSAGE_LENGTH = 2048;
 #define Log_Error( Channel, Fmt, ... ) InternalMsg( Channel, Fmt, ##__VA_ARGS__ )
 
 // Unused, just makes stuff cleaner to not have ifdef spam.
+#if !defined(GAME_GMOD_64X)
 enum collisionhints
 {
     COLLISION_HINT_DEBRIS = 0x0001,
     COLLISION_HINT_STATICSOLID = 0x0002,
 };
+#endif
 
 class IPhysicsCollisionSet;
 class IPhysics;
